530f278329d623f7642be13e230d7330cd218362
manybean <manybean00@gmail.com>
2025-11-04 11:15:02 +0900

    부트스트랩 학습 목차

---
+++ miero/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>미에로화이버 반응형웹</title>
+  
+  <!-- css초기화 -->
+  <link href="https://manybean.dothome.co.kr/miero/css/reset.css" rel="stylesheet" type="text/css">
+  <!-- 공통 서식 -->
+  <link href="https://manybean.dothome.co.kr/miero/css/common.css" rel="stylesheet" type="text/css">
+  <!-- 아이콘 - 폰트어썸 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
+
+  <!-- 서브페이지 서식 -->
+  <link href="https://manybean.dothome.co.kr/miero/css/sub.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+  <!-- 상단헤더 -->
+  <header>
+    <article class="h_top_wrap">
+      <h1>
+        <a href="index.html" title="메인페이지로이동">
+          <img src="https://manybean.dothome.co.kr/miero/img/logo2.png" alt="">
+        </a>
+      </h1>
+      <!-- 메인메뉴 -->
+      <nav class="gnb">
+        <ul>
+          <li><a href="sub1_1.html" title="미에로">미에로<i class="fas fa-angle-down"></i></a>
+            <ul class="sub">
+              <li><a href="sub1_1.html" title="브랜드 스토리">브랜드 스토리</a></li>
+              <li><a href="sub1_2.html" title="식이섬유 스토리">식이섬유 스토리</a></li>
+            </ul>
+          </li>
+          <li><a href="sub2_1.html" title="제품소개">제품소개<i class="fas fa-angle-down"></i></a>
+            <ul class="sub">
+              <li><a href="sub2_1.html" title="미에로화이바">미에로화이바</a></li>
+              <li><a href="sub2_2.html" title="미에로화이바 SPARKLING SERO SUGAR">미에로화이바 SPARKLING SERO SUGAR</a></li>
+              <li><a href="sub2_3.html" title="헬씨올리고">헬씨올리고</a></li>
+            </ul>
+          </li>
+          <li><a href="sub3_1.html" title="미에로 스토리">미에로 스토리<i class="fas fa-angle-down"></i></a>
+            <ul class="sub">
+              <li><a href="sub3_1.html" title="TV광고">TV광고</a></li>
+              <li><a href="sub3_2.html" title="인쇄광고">인쇄광고</a></li>
+              <li><a href="sub3_3.html" title="프로모션">프로모션</a></li>
+            </ul>
+          </li>
+          <li><a href="https://manybean.dothome.co.kr/board/bbs/board.php?bo_table=me_event" title="미에로 이벤트">미에로 이벤트<i class="fas fa-angle-down"></i></a>
+            <ul class="sub">
+              <li><a href="https://manybean.dothome.co.kr/board/bbs/board.php?bo_table=me_event" title="진행중인 이벤트">진행중인 이벤트</a></li>
+              <li><a href="#" title="당첨자 발표">당첨자 발표</a></li>
+            </ul>
+          </li>
+          <li><a href="../board/bbs/board.php?bo_table=me_notice" title="공지사항">공지사항</a></li>
+          <li class="shop_box"><a href="https://hyundaipharmshop.com/" title=""><i class="fas fa-shopping-bag"></i>공식쇼핑몰</a></li>
+        </ul>
+      </nav>
+    </article>
+    
+  </header>
